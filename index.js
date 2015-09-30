18130c5621005110054a89643cb33470efb2fbcb
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2015-10-01 09:24:05 +1000

    Moved build function to a sep module.

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-/* jshint ignore: start */
-var _ = require('lodash');
-var proc = require('process');
-var path = require('path');
-var through = require('through2');
-var parseConfig = require('./lib/parseConfig');
-var compile = require('./lib/compile');
-var buildConfig = require('./lib/buildConfig');
+var build = require('./lib/build');
 
-/*
-Main entrypoint to our plugin.
-*/
-function jspmBuild(options) {
-    options = options || {};
-    var configFilePath = path.join(proc.cwd(), options.config || 'config.js'),
-        stream = through.obj();
-
-    options.config = parseConfig(configFilePath);
-
-    compile(options)
-        .then(function(result) {
-            _.each(result.files, function(file) {
-                stream.write(file);
-            });
-            stream.write(buildConfig(result.config));
-            stream.end();
-        })
-        .catch(function(e){
-            stream.destroy(e);
-        });
-
-    return stream;
-}
-
-module.exports = jspmBuild;
-/* jshint ignore: end */
+module.exports = build;
