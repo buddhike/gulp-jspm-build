6d4143a093950d3162f14e64b95dc564b75edb0c
Mike Yoon <mike.yoon@modeanalytics.com>
2016-02-03 18:13:04 -0800

    Use stable stringify Sort bundle lists Add test for stable configs

--- spec/buildConfigSpec.js
+++ spec/buildConfigSpec.js
@@ -6,4 +6,12 @@ describe('buildConfig', function() {
         var f = buildConfig( config );
         expect(f.contents.toString()).toBe('System.config(' + JSON.stringify(config, null, 4) + ');');
     });
+    
+    it('should create a stably-ordered config file', function() {
+        var config1 = { a: 'b', bundles: { 'bundle.js': ['something.js', 'another.js']} };
+        var config2 = { bundles: { 'bundle.js': ['another.js', 'something.js']}, a: 'b' };
+        var f1 = buildConfig( config1 );
+        var f2 = buildConfig( config2 );
+        expect(f1.contents.toString()).toBe(f2.contents.toString());
+    });
 });
